53a57d7947a72d7c1a6bd9f9567fffb6e65a7927
Erel Segal-Halevi cron <erelsgl@gmail.com>
2022-12-06 14:13:40 +0200

    round

--- 06-item-allocation-with-money/code/rent-calculation.py
+++ 06-item-allocation-with-money/code/rent-calculation.py
@@ -7,7 +7,7 @@ AUTHOR: Erel Segal-Halevi
 SINCE:  2019-11
 """
 
-import cvxpy
+import cvxpy, numpy as np
 
 
 print("\nFinding rents for maximum-value matching {('salon', 'aya'), ('heder', 'batya'), ('martef', 'gila')}:")
@@ -16,22 +16,23 @@ price_martef, price_heder, price_salon = cvxpy.Variable(), cvxpy.Variable(), cvx
 prob = cvxpy.Problem(
     cvxpy.Minimize(0),
     # cvxpy.Maximize(price_martef+price_heder+price_salon),
-    constraints = [price_martef + price_heder + price_salon == 100,
+    constraints = [price_martef + price_heder + price_salon == 1000,
                    # price_martef >= 0, price_heder >= 0, price_salon >= 0,
-            35 - price_salon >= 40 - price_heder,
-            35 - price_salon >= 25 - price_martef,  # aya does not envy
+            350 - price_salon >= 400 - price_heder,
+            350 - price_salon >= 250 - price_martef,  # aya does not envy
 
-            60 - price_heder >= 35 - price_salon,
-            60 - price_heder >= 40 - price_martef,  # batya does not envy
+            600 - price_heder >= 350 - price_salon,
+            600 - price_heder >= 400 - price_martef,  # batya does not envy
 
-            20 - price_martef >= 40 - price_heder,
-            20 - price_martef >= 25 - price_salon,  # gila does not envy
+            200 - price_martef >= 400 - price_heder,
+            200 - price_martef >= 250 - price_salon,  # gila does not envy
     ]
 )
 prob.solve()
 print("status: ", prob.status)
 print("optimal value: ", prob.value)
-print("rents: heder={}, martef={}, salon={}".format(price_heder.value, price_martef.value, price_salon.value))
+print(f"rents: heder={np.round(price_heder.value)}, martef={np.round(price_martef.value)}, salon={np.round(price_salon.value)}")
+
 
 print("\nFinding rents for another matching {('salon', 'batya'), ('heder', 'aya'), ('martef', 'gila')}")
 price_martef, price_heder, price_salon = cvxpy.Variable(), cvxpy.Variable(), cvxpy.Variable()
